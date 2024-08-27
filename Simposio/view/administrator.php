848b967493e4ca0d26376cc626513eafc9820e89
mat_gregorio <mateusgregorio178@gmail.com>
2024-08-27 16:16:08 -0300

    algumas alteracoes no bd

--- Simposio/view/administrator.php
+++ Simposio/view/administrator.php
@@ -1,32 +1,36 @@
 <?php
 session_start();
 ?>
+
 <head>
-<link rel="stylesheet" type="text/css" href="../assets/css/style_administrator.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style_administrator.css">
 </head>
+
 <body>
     <h2>Área administrativa</h2>
     <ul>
-    <li><a href="#" onclick="carregarConteudo('approve_teachers.php')">Aprovar Professores</a></li>
-    <li><a href="#" onclick="carregarConteudo('edit_email_templates.php')">Editar email automático</a></li>  
-    <li><a href="#" onclick="carregarConteudo('edit_presentation.php')">Editar apresentação</a></li>
-    <?php if(!isset($_SESSION['user_type']) && ($_SESSION['user_type'] == 1)){?>
-           
-    <?php } ?>    
-    </ul>   
+        <?php if (!isset($_SESSION['user_type']) || ($_SESSION['user_type'] == 1)) { ?>
+            <li><a href="#" onclick="carregarConteudo('approve_teachers.php')">Aprovar Professores</a></li>
+            <li><a href="#" onclick="carregarConteudo('edit_email_templates.php')">Editar email automático</a></li>
+            <li><a href="./edit_presentation.php">Editar apresentação</a></li>
+        <?php } ?>
+    </ul>
     <script>
-        function carregarConteudo(pagina){
+        function carregarConteudo(pagina) {
             var xhr = new XMLHttpRequest();
             xhr.open('GET', pagina, true);
-            xhr.onreadystatechange = function(){
-                if(xhr.readyState == 4 && xhr.status == 200){
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
                     document.getElementById('content').innerHTML = xhr.responseText;
-                }   
+                }
             };
             xhr.send();
         }
     </script>
 
-    
+
 </body>
+
 </html>
--- Simposio/view/edit_presentation.php
+++ Simposio/view/edit_presentation.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['user_type']) || ($_SESSION['user_type'] != 1 && $_SESSION['user_type'] != 2)){
+if (!isset($_SESSION['user_type']) || ($_SESSION['user_type'] != 1 && $_SESSION['user_type'] != 2)) {
     header("Location: ./home.php");
     exit();
 }
@@ -12,38 +12,53 @@ if (!isset($presentation)) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" type="text/css" href="../assets/css/style_home.css">
     <meta charset="UTF-8">
     <title>Editar Apresentação</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-  <script>
-    tinymce.init({
-      selector: '#meuEditor',
-      plugins: 'a11ychecker advcode advlist link lists checklist media mediaembed pageembed powerpaste table tinymcespellchecker',
-      toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link image',
-      menubar: false,
-      breading: false
-    });
-  </script>
+    <script>
+        tinymce.init({
+            selector: '#meuEditor',
+            plugins: 'a11ychecker advcode advlist link lists checklist media mediaembed pageembed powerpaste table tinymcespellchecker',
+            toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link image',
+            menubar: false,
+            breading: false
+        });
+    </script>
 </head>
+
 <body>
-    <h2>Editar Apresentação</h2>
-    <?php
-    if(isset($_SESSION['message'])){
-        echo "<div class='message'>" . $_SESSION['message'] . "</div>";
-        unset($_SESSION['message']);
-    }
-    if(isset($_SESSION['error_message'])){
-        echo "<div class='error_message'>" . $_SESSION['error_message'] . "</div>";
-        unset($_SESSION['error_message']);
-    }
-    ?>
-    <form method="post" action="../controller/presentationController.php?action=edit">
-        <textarea id="meuEditor" name="content" rows="15" cols="80"><?php echo htmlspecialchars($presentation['content']);?></textarea>
-        <br>
-        <button type="submit">Salvar</button>
-    </form>
-    <a href="../view/presentation.php">Cancelar</a>
+    <div class="container">
+        <nav class="sidebar">
+            <?php include 'menu.php'; ?>
+        </nav>
+        <div class="content" id="content">
+            <h2>Editar Apresentação</h2>
+            <?php
+            if (isset($_SESSION['message'])) {
+                echo "<div class='message'>" . $_SESSION['message'] . "</div>";
+                unset($_SESSION['message']);
+            }
+            if (isset($_SESSION['error_message'])) {
+                echo "<div class='error_message'>" . $_SESSION['error_message'] . "</div>";
+                unset($_SESSION['error_message']);
+            }
+            ?>
+            <form method="post" action="../controller/presentationController.php?action=edit">
+                <textarea id="meuEditor" name="content" rows="15" cols="80"><?php echo htmlspecialchars($presentation['content']); ?></textarea>
+                <br>
+                <button type="submit">Salvar</button>
+            </form>
+            <a href="../view/presentation.php">Cancelar</a>
+        </div>
+    </div>
+    <?php if (!isset($_SESSION['user_type'])) { ?>
+        <h2>Você não está logado</h2>
+        <?php include './login.php'; ?>
+    <?php } ?>
 </body>
+
 </html>
